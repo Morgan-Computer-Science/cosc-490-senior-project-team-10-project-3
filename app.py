0cdb1a9520eb523b89ba964e15d88a4c21ae6ce6
theo-17-hg <theo3217@gmail.com>
2026-04-20 20:17:14 +0000

    commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import sqlite3
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 app = Flask(__name__)
 CORS(app)
